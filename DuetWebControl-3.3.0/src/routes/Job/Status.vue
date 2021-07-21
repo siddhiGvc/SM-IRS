2a28e40b25b1879a644759958df0002735b6c489
ranmalesunil <ranmalesunil@gmail.com>
2021-07-22 01:48:53 +0530

    remove commented part of Toolfans

--- DuetWebControl-3.3.0/src/routes/Job/Status.vue
+++ DuetWebControl-3.3.0/src/routes/Job/Status.vue
@@ -46,11 +46,9 @@
 				</v-row>
 
 				<v-row class="flex-grow-0 flex-shrink-1 hidden-sm-and-up mt-3">
-					<!--
 					<v-col cols="6" md="6">
 						<fans-panel></fans-panel>
 					</v-col>
-					-->
 					<v-col cols="6" md="6">
 						<speed-factor-panel></speed-factor-panel>
 					</v-col>
